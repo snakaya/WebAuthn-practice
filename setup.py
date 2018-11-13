5598387b8663165221d667370bfa0af9f2440e47
Seiji Nakaya <snakaya@loosedays.jp>
2018-11-13 12:23:08 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,24 @@
-import codecs
-import os
-import re
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-
-def read(*parts):
-    with codecs.open(os.path.join(HERE, *parts), 'r') as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-
-LONG_DESCRIPTION = read('README.md')
-VERSION = find_version('webauthn', '__init__.py')
-
+VERSION = '0.8'
 
 setup(
-    name='webauthn-practice',
-    packages=['webauthn-practice'],
-    include_package_data=True,
-    version=VERSION,
-    description='A Testing Platform for WebAuthn Authenticator and Brawser.',
-    long_description=LONG_DESCRIPTION,
-    author='Seiji Nakaya@LOOSEDAYS',
-    author_email='snakaya@loosedays.jp',
-    url='https://github.com/snakaya/WebAuthn-practice',
-    download_url='https://github.com/snakaya/WebAuthn-practice/archive/'
-                 '{}.tar.gz'.format(VERSION),
+    name = 'webauthn-practice',
+    packages = ['webauthn-practice'],
+    include_package_data = True,
+    version = VERSION,
+    description = 'A Testing Platform for WebAuthn Authenticator and Brawser.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author = 'Seiji Nakaya / LOOSEDAYS',
+    author_email = 'snakaya@loosedays.jp',
+    url = 'https://github.com/snakaya/WebAuthn-practice',
+    download_url = 'https://github.com/snakaya/WebAuthn-practice/archive/'
+                   '{}.tar.gz'.format(VERSION),
+    keywords = 'webauthn FIDO2 Flask Developing Tool',
     license='BSD',
     classifiers=[
         'Intended Audience :: Developers',
@@ -44,6 +26,9 @@ setup(
         'Programming Language :: Python'
     ],
     install_requires=[
+        'Flask>=1.0.2',
+        'Flask-SQLAlchemy>=2.1',
+        'SQLAlchemy>=1.0.13',
         'cbor2>=4.0.1',
         'cryptography>=2.3.1',
         'pyOpenSSL>=18.0.0',
